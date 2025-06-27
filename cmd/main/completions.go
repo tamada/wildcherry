c4a0840bff4d982c1cb48e07fd66ff28d96f9179
Haruaki Tamada <tamada@users.noreply.github.com>
2025-06-27 13:14:23 +0900

    refactor: extract directory creation and completion generation into separate functions

--- cmd/main/completions.go
+++ cmd/main/completions.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 
@@ -8,18 +9,56 @@ import (
 	"github.com/spf13/pflag"
 )
 
+func makeDirs() error {
+	var errs []error
+	if err := os.MkdirAll("completions/bash", 0755); err != nil {
+		errs = append(errs, err)
+	}
+	if err := os.MkdirAll("completions/zsh", 0755); err != nil {
+		errs = append(errs, err)
+	}
+	if err := os.MkdirAll("completions/fish", 0755); err != nil {
+		errs = append(errs, err)
+	}
+	if err := os.MkdirAll("completions/ps1", 0755); err != nil {
+		errs = append(errs, err)
+	}
+	if len(errs) > 0 {
+		return errors.Join(errs...)
+	}
+	return nil
+}
+
+func generate(command *cobra.Command, commandName string) error {
+	var errs []error
+	if err := command.GenBashCompletionFileV2(filepath.Join("completions/bash", commandName), true); err != nil {
+		errs = append(errs, err)
+	}
+	if err := command.GenZshCompletionFile(filepath.Join("completions/zsh", commandName)); err != nil {
+		errs = append(errs, err)
+	}
+	if err := command.GenFishCompletionFile(filepath.Join("completions/fish", commandName), true); err != nil {
+		errs = append(errs, err)
+	}
+	if err := command.GenPowerShellCompletionFile(filepath.Join("completions/ps1", commandName)); err != nil {
+		errs = append(errs, err)
+	}
+	if len(errs) > 0 {
+		return errors.Join(errs...)
+	}
+	return nil
+}
+
 func generateCompletions(flag *pflag.FlagSet, commandName string) error {
 	command := &cobra.Command{
 		Use: "completions",
 	}
 	command.Flags().AddFlagSet(flag)
-	os.MkdirAll("completions/bash", 0755)
-	os.MkdirAll("completions/zsh", 0755)
-	os.MkdirAll("completions/fish", 0755)
-	os.MkdirAll("completions/ps1", 0755)
-	command.GenBashCompletionFileV2(filepath.Join("completions/bash", commandName), true)
-	command.GenZshCompletionFile(filepath.Join("completions/zsh", commandName))
-	command.GenFishCompletionFile(filepath.Join("completions/fish", commandName), true)
-	command.GenPowerShellCompletionFile(filepath.Join("completions/ps1", commandName))
+	if err := makeDirs(); err != nil {
+		return err
+	}
+	if err := generate(command, commandName); err != nil {
+		return err
+	}
 	return nil
 }
